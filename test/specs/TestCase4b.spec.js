b314ac3edfd5d379c0a7ce9774503b678c637251
Jesse Hui <jesse.hui@ii.co.uk>
2025-12-05 10:29:11 +0000

    Add TestCase4b with page object pattern

---
+++ test/specs/TestCase4b.spec.js
@@ -0,0 +1,29 @@
+const signup = require('../pageobjects/signup.page')
+const loginPage = require('../pageobjects/login.page')
+const { expect, browser, $ } = require('@wdio/globals')
+
+describe('Demo TestCase1 test', () => {
+    it('my first test', async () => {
+
+        // *** Navigate to the website ***
+        await signup.open()
+        await signup.acceptCookies()
+        await expect(await signup.slider).toBeDisplayed()
+        await browser.pause(2000)
+        
+
+        // *** click on Signup/Login button ***
+        // *** Fill in login form ***
+        await loginPage.login('jesse.hui+074@ii.co.uk', '1234567890aB')
+        await browser.pause(5000)
+
+
+        // *** Check logged in as text ***
+        await loginPage.isLoggedInAs('Jesse', 5000)
+
+
+        // *** Click logout button ***
+        await loginPage.logout (5000)
+        await loginPage.isOnLoginPage (5000)
+})
+})
