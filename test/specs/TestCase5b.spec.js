8fa064bec0aa7eab5b94757ba742a81931e89c75
Jesse Hui <jesse.hui@ii.co.uk>
2025-12-05 10:57:07 +0000

    Fix TestCase5b - remove accountInfoHeading check to handle duplicate email error

---
+++ test/specs/TestCase5b.spec.js
@@ -0,0 +1,25 @@
+const signup = require('../pageobjects/signup.page')
+const loginPage = require('../pageobjects/login.page')
+const { expect, browser, $ } = require('@wdio/globals')
+
+describe('Demo TestCase1 test', () => {
+    it('my first test', async () => {
+        
+        // *** Navigate to the website ***
+        await signup.open()
+        await signup.acceptCookies()
+        await expect(await signup.slider).toBeDisplayed()
+        await browser.pause(2000)
+
+
+        // *** click on Signup/Login button ***
+        // *** Fill in the signup form ***
+        await signup.signup('Jesse', 'jesse.hui+072@ii.co.uk')
+
+
+        // *** Check Email already exists text ***
+                await expect(await loginPage.emailAlreadyExistsError).toBeDisplayed()
+        await browser.pause(3000)
+                
+})
+})
